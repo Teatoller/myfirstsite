80a5e0d801139e9876e9f4f1898d122196dbdcd2
Teatoller <steven.otieno1@gmail.com>
2019-03-16 15:45:46 +0300

    (layout title): implement @yield on title
    - modify layout title
    
    - add @yield to layout title
    
    (#010)

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,16 +3,17 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Contacts</title>
+    <title>@yield('title')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css">
     <script src="main.js"></script>
 </head>
 <body>
-   @yield('content')
   <ul>
+  <li><a href="/">Home</a></li>
   <li><a href="/about">About Us</a></li>
   <li><a href="/contact">Contact</a></li>
   </ul>
+  @yield('content')
 </body>
 </html>
