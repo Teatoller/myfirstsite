9064ab3a941456fcabdeb31be19cbe70702991bb
Teatoller <steven.otieno1@gmail.com>
2019-03-16 15:41:30 +0300

    (extend about): implement extends in about
    - modify about page script
    - add extends from layout in about page
    
    (#008)

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,23 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Contacts</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
-    <script src="main.js"></script>
-</head>
-<body>
-  <h1>About us</h1>
+@extends('layout')
+
+@section('content')
+
+<h1>About Us</h1>
   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, 
   assumenda deleniti? Est similique vero officiis pariatur, modi non aut 
   ipsum nobis neque perferendis sapiente sit culpa facilis aliquid dignissimos 
   consequatur.
   </p>
-  <ul>
-  <li><a href="/about">About Us</a></li>
-  <li><a href="/contact">Contact</a> us to learn more</li>
-  </ul>
-</body>
-</html>
+
+@endsection
