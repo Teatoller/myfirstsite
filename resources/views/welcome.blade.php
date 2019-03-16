d460ec9175b47c8a9732ec74ea31917d10e35820
Teatoller <steven.otieno1@gmail.com>
2019-03-16 15:24:16 +0300

    (welcome page): implement extension to welcome page
    - modify welcome page script
    - add extends layout to welcome page
    
    (#007)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
-    <script src="main.js"></script>
-</head>
-<body>
-  <h1>Hello World!</h1>
-  <ul>
-  <li><a href="/about">About Us</a></li>
-  <li><a href="/contact">Contact</a></li>
-  </ul>
-</body>
-</html>
+@extends('layout')
+
+@section('content')
+
+<h1>My First Website!</h1>
+
+@endsection
